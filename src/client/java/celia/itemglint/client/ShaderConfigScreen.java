ba8fb020b469167f076d77bc56047a84ca9dd9be
CeliaClaire <119377002+seliaYYDS@users.noreply.github.com>
2026-03-28 17:37:36 +0800

    Delete src directory

--- src/client/java/celia/itemglint/client/ShaderConfigScreen.java
+++
@@ -1,9 +0,0 @@
-package celia.itemglint.client;
-
-import net.minecraft.client.gui.screens.Screen;
-
-public class ShaderConfigScreen extends ShaderQuickConfigScreen {
-    public ShaderConfigScreen(Screen parent) {
-        super(parent);
-    }
-}
